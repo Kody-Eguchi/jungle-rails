297988299c06061a8f3299603b465b21815615c2
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-06 15:13:51 -0400

    Add create new account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def index
+    @user
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to '/', notice: 'User created!'
+    else
+      redirect_to '/users/index'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password
+    )
+  end
+
 end
