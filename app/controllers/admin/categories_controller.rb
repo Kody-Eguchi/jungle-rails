c08520302ce9d69ed8031eb7d24a18790ff0bc63
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-06 10:52:08 -0400

    Add a function to create a new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,12 +3,25 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.order(:id).all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def category_params
     params.require(:category).permit(
-      :name,
-      :id
+      :name
     )
   end
 end
